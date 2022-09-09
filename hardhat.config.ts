f25424888eab45d4e8d7c44b3988de335e15aa09
tt-sportsmanager <tt2007112@gmail.com>
2022-09-10 00:09:27 +0700

    chore: add goerli configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,9 @@
 import "@nomiclabs/hardhat-waffle";
 import '@nomiclabs/hardhat-etherscan';
-import '@typechain/hardhat';
+import './tasks/deploy-all';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -45,6 +47,14 @@ module.exports = {
       live: false,
       saveDeployments: true,
       tags: ["test", "local"],
+    },
+    goerli: {
+      //url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      //url: `https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161`,
+      url: `https://patient-clean-dust.ethereum-goerli.discover.quiknode.pro/f332f155d79a962383d0b41351408de714243858/`,
+      accounts: process.env.MNEMONIC
+        ? { mnemonic: process.env.MNEMONIC }
+        : [process.env.WALLET_PRIVATE_KEY!].filter(Boolean),
     },
   },
   solidity: {
